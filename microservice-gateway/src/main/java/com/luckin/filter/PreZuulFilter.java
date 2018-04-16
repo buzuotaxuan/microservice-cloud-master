30f733715b008573e47692f1abf1714efce2a412
devy <buzuotaxuan@163.com>
2018-04-16 16:07:46 +0800

    import application-dev,pro,test,bootstrap.yml

--- microservice-gateway/src/main/java/com/luckin/filter/PreZuulFilter.java
+++ microservice-gateway/src/main/java/com/luckin/filter/PreZuulFilter.java
@@ -1,14 +1,32 @@
 package com.luckin.filter;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 
+import com.luckin.core.CoreHeaderInterceptor;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
 public class PreZuulFilter extends ZuulFilter {
+
+	private static final HashMap<String, String> TOKEN_LABEL_MAP = new HashMap<>();
+
+	static {
+		TOKEN_LABEL_MAP.put("emt", "EN,Male,Test");
+		TOKEN_LABEL_MAP.put("eft", "EN,Female,Test");
+		TOKEN_LABEL_MAP.put("cmt", "CN,Male,Test");
+		TOKEN_LABEL_MAP.put("cft", "CN,Female,Test");
+		TOKEN_LABEL_MAP.put("em", "EN,Male");
+		TOKEN_LABEL_MAP.put("ef", "EN,Female");
+		TOKEN_LABEL_MAP.put("cm", "CN,Male");
+		TOKEN_LABEL_MAP.put("cf", "CN,Female");
+	}
+	
   private static final Logger LOGGER = LoggerFactory.getLogger(PreZuulFilter.class);
 
   @Override
@@ -18,9 +36,16 @@ public class PreZuulFilter extends ZuulFilter {
 
   @Override
   public Object run() {
-    HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
     String host = request.getRemoteHost();
-    PreZuulFilter.LOGGER.info("请求的host:{}", host);
+	String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+   
+	String labels = TOKEN_LABEL_MAP.get(token);
+	 PreZuulFilter.LOGGER.info("请求的label:{},host:{}",labels, host);
+
+	CoreHeaderInterceptor.initHystrixRequestContext(labels); // zuul本身调用微服务
+	ctx.addZuulRequestHeader(CoreHeaderInterceptor.HEADER_LABEL, labels); // 传递给后续微服务
     return null;
   }
 
