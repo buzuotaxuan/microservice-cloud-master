ce5b72238dadbe43d535ab7a107b1eae153cb0e5
devy <buzuotaxuan@163.com>
2018-04-16 11:31:14 +0800

    add local model

--- microservice-user-provider/src/main/java/com/luckin/UserServiceApplication.java
+++ microservice-user-provider/src/main/java/com/luckin/UserServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
-
+	//"--spring.cloud.bootstrap.enabled=false" 本地开发模式 禁用注册中心
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args); 
 	}
